05be0723417e84e0895d4607b3188f78b8e0a4c0
Mark Pytel <markpytel@gmail.com>
2015-08-31 14:50:10 -0400

    states functional

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,26 @@
-var app = angular.module('flashCards', []);
+var app = angular.module('flashCards', ['ui.router']);
 
 app.value('currentFlashCards', []);
+
+app.config(function ($stateProvider){
+	$stateProvider.state('stats', {
+		url: '/stats',
+		templateUrl: '/views/statistics.html',
+		controller: 'StatsController'
+	}).state('flashcards', {
+		url: '/flashcards',
+		templateUrl: '/views/flashcards.html',
+		controller: 'MainController'
+	}).state('makecard', {
+		url: '/makecard',
+		templateUrl: '/views/makecard.html',
+		controller: 'NewCardController'
+	});
+
+});
+
+
+
+
+
+//states are statistics, flashcard view, and new flashcard form
